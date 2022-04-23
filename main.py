62a786b066d24824a411d0340a69934d39de11ff
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-04-23 23:31:55 +0300

    requirements added

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Depends
 import my_dbase
 from to_json import *
 from my_valids import *
-from pyngrok import ngrok
 from fastapi.responses import JSONResponse
 
 MY_HEADER = {"Access-Control-Allow-Origin": "*"}
@@ -27,11 +26,11 @@ async def all_hist(coin_name):
 
 
 # http://127.0.0.1:8000/coin_range?coin_name=avalanche&time_begin=2020-12-01-00-00-00&time_end=2020-12-02-00-00-00
-@app.get("/coin_range")
-async def coin_range(params: History_Range_Param_Valid = Depends(History_Range_Param_Valid)):
-    hist = my_dbase.get_range_coin_hist_by_time(params)
-    my_json = to_json_range_coin(params, hist)
-    return JSONResponse(content=my_json, headers=MY_HEADER)
+# @app.get("/coin_range")
+# async def coin_range(params: History_Range_Param_Valid = Depends(History_Range_Param_Valid)):
+#     hist = my_dbase.get_range_coin_hist_by_time(params)
+#     my_json = to_json_range_coin(params, hist)
+#     return JSONResponse(content=my_json, headers=MY_HEADER)
 
 
 # http://127.0.0.1:8000/coin_page?coin_name=bitcoin&page=1&n_rows=20
@@ -43,11 +42,11 @@ async def coin_page(params: History_Page_Param_Valid = Depends(History_Page_Para
 
 
 # http://127.0.0.1:8000/news_range?time_begin=2020-12-01-00-00-00&time_end=2020-12-02-00-00-00
-@app.get("/news_range")
-async def news_range(params: News_Range_Param_Valid = Depends(News_Range_Param_Valid)):
-    news = my_dbase.get_range_news_hist_by_time(params)
-    my_json = to_json_range_news(params, news)
-    return JSONResponse(content=my_json, headers=MY_HEADER)
+# @app.get("/news_range")
+# async def news_range(params: News_Range_Param_Valid = Depends(News_Range_Param_Valid)):
+#     news = my_dbase.get_range_news_hist_by_time(params)
+#     my_json = to_json_range_news(params, news)
+#     return JSONResponse(content=my_json, headers=MY_HEADER)
 
 
 # http://127.0.0.1:8000/news_page?page=1&n_rows=20
