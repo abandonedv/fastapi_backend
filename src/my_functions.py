74265fcddc2b53098672b88a5b9e083d2b8bf117
Вадим Ильиных <vadimiiofficial@mail.ru>
2022-07-04 10:42:41 +0300

    list generator update

--- src/my_functions.py
+++ src/my_functions.py
@@ -3,7 +3,8 @@ import my_dbase
 
 def get_names_obj_list():
     names = my_dbase.get_all_coin_names()
-    names_obj_list = []
-    for name in names:
-        names_obj_list.append(my_dbase.get_last_update_of_coin(name))
+    names_obj_list = [my_dbase.get_last_update_of_coin(name)
+                      for name in names]
+    # for name in names:
+    #     names_obj_list.append(my_dbase.get_last_update_of_coin(name))
     return names_obj_list
--- src/to_json.py
+++ src/to_json.py
@@ -4,11 +4,14 @@ import pprint
 def to_json_names(names):
     my_json = {}
 
-    new_list_of_names = []
-    for name in names:
-        new_list_of_names.append({"coin_name": name.coin_parse,
-                                  "coin_value": name.coin_value,
-                                  "coin_time": name.coin_time})
+    new_list_of_names = [{"coin_name": name.coin_parse,
+                          "coin_value": name.coin_value,
+                          "coin_time": name.coin_time}
+                         for name in names]
+    # for name in names:
+    #     new_list_of_names.append({"coin_name": name.coin_parse,
+    #                               "coin_value": name.coin_value,
+    #                               "coin_time": name.coin_time})
     my_json["all_names"] = new_list_of_names
     return my_json
 
@@ -16,9 +19,11 @@ def to_json_names(names):
 def to_json_all(name, list_of_prices):
     my_json = {"coin_name": name}
 
-    new_list_of_prices = []
-    for price in list_of_prices:
-        new_list_of_prices.append([price.coin_time_in_sec * 1000, price.coin_value])
+    new_list_of_prices = [[price.coin_time_in_sec * 1000, price.coin_value]
+                          for price in list_of_prices]
+
+    # for price in list_of_prices:
+    #     new_list_of_prices.append([price.coin_time_in_sec * 1000, price.coin_value])
 
     my_json["history_list"] = new_list_of_prices
     return my_json
@@ -29,9 +34,10 @@ def to_json_range_coin(params, list_of_prices):
                "time_begin": params.time_begin,
                "time_end": params.time_end}
 
-    new_list_of_prices = []
-    for price in list_of_prices:
-        new_list_of_prices.append({"coin_value": price.coin_value, "coin_time": price.coin_time})
+    new_list_of_prices = [{"coin_value": price.coin_value, "coin_time": price.coin_time}
+                          for price in list_of_prices]
+    # for price in list_of_prices:
+    #     new_list_of_prices.append({"coin_value": price.coin_value, "coin_time": price.coin_time})
 
     my_json["history_list"] = new_list_of_prices
     return my_json
@@ -42,9 +48,10 @@ def to_json_page_coin(params, list_of_prices):
                "page": params.page,
                "n_rows": params.n_rows}
 
-    new_list_of_prices = []
-    for price in list_of_prices:
-        new_list_of_prices.append({"coin_value": price.coin_value, "coin_time": price.coin_time})
+    new_list_of_prices = [{"coin_value": price.coin_value, "coin_time": price.coin_time}
+                          for price in list_of_prices]
+    # for price in list_of_prices:
+    #     new_list_of_prices.append({"coin_value": price.coin_value, "coin_time": price.coin_time})
 
     my_json["history_list"] = new_list_of_prices
     return my_json
@@ -54,11 +61,14 @@ def to_json_range_news(params, list_of_news):
     my_json = {"time_begin": params.time_begin,
                "time_end": params.time_end}
 
-    new_list_of_news = []
-    for news in list_of_news:
-        new_list_of_news.append({"news_title": news.news_title,
-                                 "news_lead": news.news_lead,
-                                 "news_time": news.news_time})
+    new_list_of_news = [{"news_title": news.news_title,
+                         "news_lead": news.news_lead,
+                         "news_time": news.news_time}
+                        for news in list_of_news]
+    # for news in list_of_news:
+    #     new_list_of_news.append({"news_title": news.news_title,
+    #                              "news_lead": news.news_lead,
+    #                              "news_time": news.news_time})
 
     my_json["news_list"] = new_list_of_news
     return my_json
@@ -68,11 +78,14 @@ def to_json_page_news(params, list_of_news):
     my_json = {"page": params.page,
                "n_rows": params.n_rows}
 
-    new_list_of_news = []
-    for news in list_of_news:
-        new_list_of_news.append({"news_title": news.news_title,
-                                 "news_lead": news.news_lead,
-                                 "news_time": news.news_time})
+    new_list_of_news = [{"news_title": news.news_title,
+                         "news_lead": news.news_lead,
+                         "news_time": news.news_time}
+                        for news in list_of_news]
+    # for news in list_of_news:
+    #     new_list_of_news.append({"news_title": news.news_title,
+    #                              "news_lead": news.news_lead,
+    #                              "news_time": news.news_time})
 
     my_json["news_list"] = new_list_of_news
     return my_json
